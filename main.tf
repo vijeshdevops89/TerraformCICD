4ffab03e47495a0760fa134bb6a9bd18b2fbc427
Vijesh Vijayan Nair <vijeshawsnair@gmail.com>
2025-02-24 15:00:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
 
 
-# Generate a Key Pair
-resource "aws_key_pair" "my_key" {
-  key_name   = "my-ec2-key"
-  public_key = file("C:/Users/Vijesh/.ssh/id_ed25519.pub")  # Use your actual public key path
-}
-
 # Create a Security Group
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2-security-group"
@@ -40,7 +34,7 @@ resource "aws_security_group" "ec2_sg" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-04b4f1a9cf54c11d0"  # Update with a valid AMI ID
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.my_key.key_name
+  key_name      = "k8s" #aws_key_pair.my_key.key_name
   security_groups = [aws_security_group.ec2_sg.name]
 
   tags = {
